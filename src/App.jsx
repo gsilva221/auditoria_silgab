db7da4a5eb53312ae9f4b1a1066ae0afe1e91ca4
unknown <sistemas@VRE_LAB20829.inacap.cl>
2026-06-23 17:36:38 -0400

    actualizando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Resumen from './components/Resumen';
+import Vulnerabilidades from './components/Vulnerabilidades';
+import Matriz from './components/Matriz';
+import Mitigacion from './components/Mitigacion';
+import './App.css'; // Asegúrate de importar tus estilos
+
+function App() {
+  return (
+    <>
+      <header>
+        <div className="logo">TurBus - Informe IA</div>
+        <nav className="topics">
+            <a href="#resumen">Resumen Ejecutivo</a>
+            <a href="#ataques">Vulnerabilidades</a>
+            <a href="#matriz">Matriz de Riesgo</a>
+            <a href="#mitigacion">Mitigación y DR</a>
+        </nav>
+      </header>
+
+      <main>
+        {/* Aquí llamas a los componentes que renderizarán tus .md */}
+        <Resumen />
+        <Vulnerabilidades />
+        <Matriz />
+        <Mitigacion />
+      </main>
+
+      <footer>
+        <p>&copy; 2026 Auditoría de Seguridad - Evaluación Unidad 3. Desarrollado en React.</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
